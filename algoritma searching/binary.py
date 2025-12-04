601641c79ef5b1b357575c0b39eac8fa8a8452cb
wiia58211-design <wiia58211@gmail.com>
2025-12-04 20:47:55 +0700

    Update binary.py

--- algoritma searching/binary.py
+++ algoritma searching/binary.py
@@ -1,1 +1,22 @@
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+            
+    return -1
+
+data = [10, 20, 30, 40, 50]
+cari = 40
+
+hasil = binary_search(data, cari)
+print("Ditemukan di indeks:", hasil)
 
