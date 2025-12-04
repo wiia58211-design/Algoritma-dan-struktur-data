d686a4cc851d6bfde61cfc41f7da8cad445fb6f9
wiia58211-design <wiia58211@gmail.com>
2025-12-04 20:17:14 +0700

    Update 1.1 Tipe data character, integer,dan floating point.py

--- Tipe Data, Array, Struktur, Unions,varibel Statis & variabel Dinamis/1.1 Tipe data character, integer,dan floating point.py
+++ Tipe Data, Array, Struktur, Unions,varibel Statis & variabel Dinamis/1.1 Tipe data character, integer,dan floating point.py
@@ -1,1 +1,32 @@
+# Contoh tipe data karakter (character)
+char1 = 'A'
+char2 = 'b'
+char3 = '9'
+
+print("Tipe Data Karakter:")
+print(char1, type(char1))
+print(char2, type(char2))
+print(char3, type(char3))
+print("---------------------------")
+
+# Contoh tipe data integer
+angka_integer = 25
+jumlah_barang = 10
+tahun = 2025
+
+print("Tipe Data Integer:")
+print(angka_integer, type(angka_integer))
+print(jumlah_barang, type(jumlah_barang))
+print(tahun, type(tahun))
+print("---------------------------")
+
+# Contoh tipe data floating point
+nilai_float = 3.14
+berat_badan = 55.7
+harga = 12.99
+
+print("Tipe Data Floating Point:")
+print(nilai_float, type(nilai_float))
+print(berat_badan, type(berat_badan))
+print(harga, type(harga))
 
